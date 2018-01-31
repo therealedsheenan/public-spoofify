4c4009ba565de2b961b2a208f8ea27c8de417893
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-01-31 16:11:01 +0800

    mac random function

---
+++ mac_random.py
@@ -0,0 +1,14 @@
+import random
+
+def randomMAC():
+    return [ 0x00, 0x16, 0x3e,
+             random.randint(0x00, 0x7f),
+             random.randint(0x00, 0xff),
+             random.randint(0x00, 0xff) ]
+
+def MACprettyprint(mac):
+    return ':'.join(map(lambda x: "%02x" % x, mac))
+
+def randomize():
+    return MACprettyprint(randomMAC())
+
